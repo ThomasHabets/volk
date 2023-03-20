875034cbc6fc33d32e0a59898fbd593ca9dc2e5d
Thomas Habets <thomas@habets.se>
2023-03-20 00:53:15 +0000

    TODO: volk_32fc_x2_dot_prod_32fc_sifive_u74

---
+++ kernels/volk/asm/riscv/volk_32fc_32f_dot_prod_32fc_sifive_u74.s
@@ -0,0 +1,58 @@
+        .text
+        .align 2
+        .type   volk_32fc_x2_dot_prod_32fc_sifive_u74, @function
+        .global volk_32fc_x2_dot_prod_32fc_sifive_u74
+
+volk_32fc_x2_dot_prod_32fc_sifive_u74:
+	# a0: out
+	# a1: in
+	# a2: taps
+	# a3: points
+	beqz a3, .done
+
+	slli a5,a3,3
+	add  a5,a5,a1
+
+	# a5: one past the end of input
+
+	fmv.w.x ft0,zero
+	fmv.w.x ft1,zero
+	fmv.w.x ft7,zero
+	fmv.w.x ft8,zero
+
+	fmv.w.x ft11,zero
+
+	# ft0/ft1: res
+
+.loop:
+	# load in
+	flw  ft3,4(a1) # in1
+	flw  ft2,0(a1) # in0
+	flw  ft4,0(a2) # tp0
+	flw  ft5,4(a2) # tp1
+
+	fsub.s  ft6,ft11,ft3    # neg in1
+	fmadd.s ft0,ft2,ft4,ft0 # n0*tp0
+	addi a1,a1,8
+	fmadd.s ft1,ft2,ft5,ft1 # n0*tp1
+	addi a2,a2,8
+	fmadd.s ft7,ft3,ft4,ft7 # n1*tp0
+	fmadd.s ft8,ft6,ft5,ft8 # -in1*tp1
+	
+	ble a1, a5, .loop
+	beq a1,a5,.done # TODO: branch predict assumes not taken
+	# TODO: handle odd case
+.done:
+	fadd.s ft0,ft0,ft8
+	fadd.s ft1,ft1,ft7
+	fsw ft0,0(a0)
+	fsw ft1,4(a0)
+	ret
+
+#volk_32fc_32f_dot_prod_32fc_a:
+#volk_32fc_32f_dot_prod_32fc_a:
+#volk_32fc_32f_dot_prod_32fc_a_sifive_u74:	
+#volk_32fc_x2_dot_prod_32fc_a:
+#volk_32f_x2_dot_prod_16i_a:	
+#volk_32f_x2_dot_prod_16i_a_sifive_u74:
+#volk_32f_x2_dot_prod_32f_a:	
