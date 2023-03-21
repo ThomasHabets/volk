e4583ff9e55655ec38189fd9a73c9ce89996ee92
Thomas Habets <thomas@habets.se>
2023-03-21 23:28:06 +0000

    2x unroll

--- kernels/volk/asm/riscv/volk_32fc_32f_dot_prod_32fc_sifive_u74.s
+++ kernels/volk/asm/riscv/volk_32fc_32f_dot_prod_32fc_sifive_u74.s
@@ -22,38 +22,70 @@ volk_32fc_x2_dot_prod_32fc_sifive_u74:
 
 	# a5: one past the end of input
 
+	# Output regs.
 	fmv.w.x ft0,zero
 	fmv.w.x ft1,zero
+	fmv.w.x ft2,zero
+	fmv.w.x ft3,zero
+	fmv.w.x ft4,zero
+	fmv.w.x ft5,zero
+	fmv.w.x ft6,zero
 	fmv.w.x ft7,zero
-	fmv.w.x ft8,zero
 
-	fmv.w.x ft11,zero
+	addi     a1,a1,16          # free ride in pipeline A.
+	addi     a2,a2,16          # free ride in pipeline A.
+	bgt      a1,a5,.endloop
 
-	# ft0/ft1 and ft8/ft7: res
-	# split into two to avoid write-read stall in inner loop.
-
+	# Main loop two complexes at a time.
 .loop:
 	# Load input in order of when it'll be used.
 	# flw has 2 cycle latency, 1 cycle repeat.
-	flw  ft2,0(a1) # in0
-	flw  ft4,0(a2) # tp0
-	flw  ft5,4(a2) # tp1
-	flw  ft3,4(a1) # in1
+	flw  ft8,-16(a1) # in0
+	flw  ft9,-16(a2) # tp0
+	flw  ft10,-12(a2) # tp1
+	flw  ft11,-12(a1) # in1
 
 	# None of the fused multiple-adds have a write-read stall.
 	# FMA, like mul and add, have 5 cycle latency, 1 cycle repeat.
-	fmadd.s ft0,ft2,ft4,ft0  # n0*tp0
-	addi a1,a1,8             # free ride in pipeline A.
-	fmadd.s ft1,ft2,ft5,ft1  # n0*tp1
-	addi a2,a2,8             # free ride in pipeline A.
-	fmadd.s ft7,ft3,ft4,ft7  # n1*tp0
-	fnmsub.s ft8,ft3,ft5,ft8 # -in1*tp1
-	
-	ble a1, a5, .loop
+	fmadd.s  ft0,ft8, ft9, ft0 # in0*tp0
+	flw  fa0,-8(a1)            # in0
+	fmadd.s  ft1,ft8, ft10,ft1 # in0*tp1
+	flw  fa1,-8(a2)            # tp0
+	fmadd.s  ft2,ft11,ft9, ft2 # in1*tp0
+	flw  fa2,-4(a2)            # tp1
+	fnmsub.s ft3,ft11,ft10,ft3 # -in1*tp1
+	flw  fa3,-4(a1)            # in1
 
-	# TODO: branch predict assumes not taken, but almost always will.
-	# Not worth optimizing since only one mispredict per call?
-	beq a1,a5,.done
+	fmadd.s  ft4,fa0,fa1,ft4   # in0*tp0
+	addi     a1,a1,16          # free ride in pipeline A.
+	fmadd.s  ft5,fa0,fa2,ft5   # in0*tp1
+	addi     a2,a2,16          # free ride in pipeline A.
+	fmadd.s  ft6,fa3,fa1,ft6   # in1*tp0
+	fnmsub.s ft7,fa3,fa2,ft7   # -in1*tp1
+	blt a1,a5,.loop
+
+.endloop:
+	beq a1,a5, .done
+
+	# Do one more complex.
+	addi     a4, a1, -12
+	addi     a1, a1, -16
+	
+	bge      a4, a5, .oddcheck
+
+	flw  fa0,0(a1) # in0
+	flw  fa1,0(a2) # tp0
+	flw  fa2,4(a2) # tp1
+	flw  fa3,4(a1) # in1
+
+	fmadd.s  ft4,fa0,fa1,ft4   # in0*tp0
+	fmadd.s  ft5,fa0,fa2,ft5   # in0*tp1
+	fmadd.s  ft6,fa3,fa1,ft6   # in1*tp0
+	fnmsub.s ft7,fa3,fa2,ft7   # -in1*tp1
+	addi a1,a1,8
+	
+.oddcheck:
+	beq  a1,a5, .done
 
 	# Handle odd number
 	flw  ft2,0(a1) # in0
@@ -62,7 +94,11 @@ volk_32fc_x2_dot_prod_32fc_sifive_u74:
 .done:
 	# Some one-time stalling here.
 	# Latency 5, repeat 1.
-	fadd.s ft0,ft0,ft8
+	fadd.s ft0,ft0,ft2
+	fadd.s ft1,ft1,ft3
+	fadd.s ft0,ft0,ft4
+	fadd.s ft1,ft1,ft5
+	fadd.s ft0,ft0,ft6
 	fadd.s ft1,ft1,ft7
 	# fsw has latency 4, repeat 1.
 	fsw ft0,0(a0)
