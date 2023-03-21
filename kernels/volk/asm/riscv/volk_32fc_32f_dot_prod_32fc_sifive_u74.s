41fed93a19c99240e5872976f5f7cb0ba9462922
Thomas Habets <thomas@habets.se>
2023-03-21 21:58:38 +0000

    Add comments
    RUN_VOLK_TESTS: volk_32fc_x2_dot_prod_32fc(131071,1987)
    sifive_u74 completed in 2241.08 ms
    generic completed in 2722.03 ms
    a_generic completed in 2731.77 ms
    Best aligned arch: sifive_u74
    Best unaligned arch: sifive_u74

--- kernels/volk/asm/riscv/volk_32fc_32f_dot_prod_32fc_sifive_u74.s
+++ kernels/volk/asm/riscv/volk_32fc_32f_dot_prod_32fc_sifive_u74.s
@@ -3,6 +3,13 @@
         .type   volk_32fc_x2_dot_prod_32fc_sifive_u74, @function
         .global volk_32fc_x2_dot_prod_32fc_sifive_u74
 
+	#
+	# RISC-V implementation using only I and F sets.
+	# About 24% faster than GCC.
+	#
+	# The generic C code is 2x unrolled, but its main flaw
+	# seems to be not properly fusing into fmadd and fnmsub.
+	#
 volk_32fc_x2_dot_prod_32fc_sifive_u74:
 	# a0: out
 	# a1: in
@@ -22,26 +29,30 @@ volk_32fc_x2_dot_prod_32fc_sifive_u74:
 
 	fmv.w.x ft11,zero
 
-	# ft0/ft1: res
+	# ft0/ft1 and ft8/ft7: res
+	# split into two to avoid write-read stall in inner loop.
 
 .loop:
-	# load input in order of when it'll be used.
+	# Load input in order of when it'll be used.
+	# flw has 2 cycle latency, 1 cycle repeat.
 	flw  ft2,0(a1) # in0
 	flw  ft4,0(a2) # tp0
 	flw  ft5,4(a2) # tp1
 	flw  ft3,4(a1) # in1
 
-	fmadd.s ft0,ft2,ft4,ft0 # n0*tp0
-	addi a1,a1,8
-	fmadd.s ft1,ft2,ft5,ft1 # n0*tp1
-	addi a2,a2,8
-	fmadd.s ft7,ft3,ft4,ft7 # n1*tp0
+	# None of the fused multiple-adds have a write-read stall.
+	# FMA, like mul and add, have 5 cycle latency, 1 cycle repeat.
+	fmadd.s ft0,ft2,ft4,ft0  # n0*tp0
+	addi a1,a1,8             # free ride in pipeline A.
+	fmadd.s ft1,ft2,ft5,ft1  # n0*tp1
+	addi a2,a2,8             # free ride in pipeline A.
+	fmadd.s ft7,ft3,ft4,ft7  # n1*tp0
 	fnmsub.s ft8,ft3,ft5,ft8 # -in1*tp1
 	
 	ble a1, a5, .loop
 
 	# TODO: branch predict assumes not taken, but almost always will.
-	# Not worth optimizing?
+	# Not worth optimizing since only one mispredict per call?
 	beq a1,a5,.done
 
 	# Handle odd number
@@ -49,8 +60,11 @@ volk_32fc_x2_dot_prod_32fc_sifive_u74:
 	flw  ft4,0(a2) # tp0
 	fmadd.s ft0,ft2,ft4,ft0
 .done:
+	# Some one-time stalling here.
+	# Latency 5, repeat 1.
 	fadd.s ft0,ft0,ft8
 	fadd.s ft1,ft1,ft7
+	# fsw has latency 4, repeat 1.
 	fsw ft0,0(a0)
 	fsw ft1,4(a0)
 	ret
